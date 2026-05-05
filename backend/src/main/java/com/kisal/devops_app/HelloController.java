c1def7d083c337dc268ffc933482de77779f9818
kisal-dv <kisalangira119@gmail.com>
2026-05-05 12:49:20 +0530

    first fullstack app

---
+++ backend/src/main/java/com/kisal/devops_app/HelloController.java
@@ -0,0 +1,15 @@
+package com.kisal.devops_app;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello DevOps 🚀";
+    }
+}
---
+++ backend/src/test/java/com/kisal/devops_app/DevopsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisal.devops_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
